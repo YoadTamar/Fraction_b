4d5dd50e90501cec4d3bea99ea1cefe95937010e
YoadTamar <119599940+YoadTamar@users.noreply.github.com>
2023-05-10 23:49:07 +0300

    Update Fraction.cpp

--- sources/Fraction.cpp
+++ sources/Fraction.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 using namespace ariel;
 
+// This function removes the greatest common divisor (gcd) from the fraction
 void ariel::Fraction::remove_gcd()
 {
     int gcd = __gcd(numerator, denominator);
@@ -18,14 +19,18 @@ void ariel::Fraction::remove_gcd()
     }
 }
 
+// Constructor that takes two integer arguments, representing the numerator and denominator of the fraction
 ariel::Fraction::Fraction(int x, int y) : numerator(x), denominator(y)
 {
+    // If the denominator is zero, throw an exception
     if (y == 0)
     {
         throw std::invalid_argument(" you divide by zero !!! ");
     }
     else
     {
+        // Check if the numerator and denominator are equal to the global maximum or minimum values
+        // If they are, set the numerator and denominator to 1 or -1 accordingly
         if ((x == global_maximum && y == global_maximum) || (x == global_minimum && y == global_minimum))
         {
             numerator = 1;
@@ -37,30 +42,41 @@ ariel::Fraction::Fraction(int x, int y) : numerator(x), denominator(y)
             denominator = 1;
         }
     }
+
+    // Remove the gcd from the fraction
     this->remove_gcd();
 }
 
+// Constructor that takes a float argument and converts it to a fraction
 ariel::Fraction::Fraction(float float_number) : Fraction(1000 * float_number, 1000) {}
 
+// Overload the "+" operator for two fractions
 ariel::Fraction ariel::Fraction::operator+(const Fraction &fraction)
 {
+    // Calculate the new numerator and denominator for the result
     long long newNum = (long long)(numerator * fraction.denominator) + (long long)(fraction.numerator * denominator);
     long long newDenom = (long long)(denominator) * (long long)(fraction.denominator);
 
+    // If the new numerator or denominator is greater than the global maximum or less than the global minimum, throw an exception
     if (newNum > global_maximum || newNum < global_minimum || newDenom > global_maximum || newDenom < global_minimum)
     {
         throw std::overflow_error(" you have overflow !!!! ");
     }
+
     return Fraction(newNum, newDenom);
 }
 
+// Overload the "+" operator for a float and a fraction
 ariel::Fraction Fraction::operator+(float float_number)
 {
+    // Convert the float to a fraction and use the previous "+" overload to add it to the current fraction
     return (*(this)) + ariel::Fraction(float_number);
 }
 
+// Overload the "+" operator for a fraction and a float
 ariel::Fraction ariel::operator+(float float_number, const Fraction &fraction)
 {
+    // Convert the float to a fraction and use the previous "+" overload to add it to the current fraction
     return ariel::Fraction(float_number) + fraction;
 }
 
